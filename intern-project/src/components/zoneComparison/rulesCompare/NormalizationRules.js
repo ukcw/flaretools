970c32cbb63f733a27adc14469a4ce9773362af0
Ulysses Kee <ulysseskcw96@gmail.com>
2022-05-17 00:51:23 +0800

    added bulk copying support for NormalizationRules

--- intern-project/src/components/zoneComparison/rulesCompare/NormalizationRules.js
+++ intern-project/src/components/zoneComparison/rulesCompare/NormalizationRules.js
@@ -1,5 +1,4 @@
 import {
-  Heading,
   Stack,
   Table,
   Tag,
@@ -69,7 +68,8 @@ const returnConditions = (data) => {
 };
 
 const NormalizationRules = (props) => {
-  const { zoneKeys, credentials, zoneDetails } = useCompareContext();
+  const { zoneKeys, credentials, zoneDetails, zoneCopierFunctions } =
+    useCompareContext();
   const [normalizationRulesData, setNormalizationRulesData] = useState();
   const {
     isOpen: ErrorPromptIsOpen,
@@ -244,6 +244,189 @@ const NormalizationRules = (props) => {
     setNormalizationRulesData();
     getData();
   };
+
+  const bulkCopyHandler = async (
+    data,
+    zoneKeys,
+    credentials,
+    setResults,
+    setProgress
+  ) => {
+    setProgress((prevState) => {
+      // trigger spinner on UI
+      const newState = {
+        ...prevState,
+        [props.id]: {
+          ...prevState[props.id],
+          completed: false,
+        },
+      };
+      return newState;
+    });
+
+    // initialize state
+    setResults((prevState) => {
+      const newState = {
+        ...prevState,
+        [props.id]: {
+          ...prevState[props.id],
+          errors: [],
+          copied: [],
+        },
+      };
+      return newState;
+    });
+
+    async function sendPostRequest(data, endpoint) {
+      const resp = await putZoneSetting(data, endpoint);
+      return resp;
+    }
+
+    async function getData() {
+      const resp = await getMultipleZoneSettings(
+        zoneKeys,
+        credentials,
+        "/url_normalization"
+      );
+      const processedResp = resp.map((zone) => zone.resp);
+      const secondaryProcessedResp = [
+        [JSON.parse(JSON.stringify(processedResp))], // this method does a deep copy of the objects
+        [JSON.parse(JSON.stringify(processedResp))], // use this for as many times as unique setting values required
+        [JSON.parse(JSON.stringify(processedResp))],
+      ];
+
+      // NORMALIZE TYPE
+      secondaryProcessedResp[0] = secondaryProcessedResp[0][0].map((res) => {
+        let newObj = { ...res };
+        newObj.result["id"] = "normalization_type";
+        return newObj;
+      });
+
+      // NORMALIZE INCOMING URLS
+      secondaryProcessedResp[1] = secondaryProcessedResp[1][0].map((res) => {
+        let newObj = { ...res };
+        newObj.result["id"] = "incoming_urls";
+        return newObj;
+      });
+
+      // NORMALIZE URLS TO ORIGIN
+      secondaryProcessedResp[2] = secondaryProcessedResp[2][0].map((res) => {
+        let newObj = { ...res };
+        newObj.result["id"] = "urls_to_origin";
+        return newObj;
+      });
+      setNormalizationRulesData(secondaryProcessedResp);
+    }
+
+    // not possible for data not to be loaded (logic is at displaying this button)
+    const baseZoneData = data[0][0].result;
+    const otherZoneKeys = zoneKeys.slice(1);
+
+    // initialize state
+    setProgress((prevState) => {
+      const newState = {
+        ...prevState,
+        [props.id]: {
+          ...prevState[props.id],
+          status: "copy",
+          totalToCopy: 1,
+          progressTotal: 0,
+          completed: false,
+        },
+      };
+      return newState;
+    });
+
+    const createData = {
+      type: baseZoneData.type,
+      scope: baseZoneData.scope,
+    };
+
+    for (const key of otherZoneKeys) {
+      const dataToCreate = _.cloneDeep(createData);
+      const authObj = {
+        zoneId: credentials[key].zoneId,
+        apiToken: `Bearer ${credentials[key].apiToken}`,
+      };
+      // setCurrentZone(key);
+      const dataWithAuth = { ...authObj, data: dataToCreate };
+      const { resp: postRequestResp } = await sendPostRequest(
+        dataWithAuth,
+        "/put/url_normalization"
+      );
+      if (postRequestResp.success === false) {
+        const errorObj = {
+          code: postRequestResp.errors[0].code,
+          message: postRequestResp.errors[0].message,
+          data: dataToCreate.value,
+        };
+        setResults((prevState) => {
+          const newState = {
+            ...prevState,
+            [props.id]: {
+              ...prevState[props.id],
+              errors: prevState[props.id]["errors"].concat(errorObj),
+            },
+          };
+          return newState;
+        });
+      } else {
+        setResults((prevState) => {
+          const newState = {
+            ...prevState,
+            [props.id]: {
+              ...prevState[props.id],
+              copied: prevState[props.id]["copied"].concat(dataToCreate),
+            },
+          };
+          return newState;
+        });
+      }
+      setProgress((prevState) => {
+        const newState = {
+          ...prevState,
+          [props.id]: {
+            ...prevState[props.id],
+            progressTotal: prevState[props.id]["progressTotal"] + 1,
+          },
+        };
+        return newState;
+      });
+    }
+    setNormalizationRulesData();
+    getData();
+
+    setProgress((prevState) => {
+      const newState = {
+        ...prevState,
+        [props.id]: {
+          ...prevState[props.id],
+          completed: true,
+        },
+      };
+      return newState;
+    });
+    return;
+  };
+
+  if (!normalizationRulesData) {
+  } // don't do anything while the app has not loaded
+  else if (
+    normalizationRulesData &&
+    normalizationRulesData[0][0].success &&
+    normalizationRulesData[0][0].result
+  ) {
+    zoneCopierFunctions[props.id] = (setResults, setProgress) =>
+      bulkCopyHandler(
+        normalizationRulesData,
+        zoneKeys,
+        credentials,
+        setResults,
+        setProgress
+      );
+  } else {
+    zoneCopierFunctions[props.id] = false;
+  }
 
   return (
     <Stack w="100%" spacing={4}>
