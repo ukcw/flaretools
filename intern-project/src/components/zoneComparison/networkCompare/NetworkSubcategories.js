7ddf5e17fc0759589b4486b9e58b1831219d490d
Ulysses Kee <ulysseskcw96@gmail.com>
2022-05-17 00:58:38 +0800

    added bulk copying support for NetworkSubcategories

--- intern-project/src/components/zoneComparison/networkCompare/NetworkSubcategories.js
+++ intern-project/src/components/zoneComparison/networkCompare/NetworkSubcategories.js
@@ -1,5 +1,4 @@
 import {
-  Heading,
   Stack,
   Table,
   Tag,
@@ -53,7 +52,8 @@ const returnConditions = (data) => {
 };
 
 const NetworkSubcategories = (props) => {
-  const { zoneKeys, credentials, zoneDetails } = useCompareContext();
+  const { zoneKeys, credentials, zoneDetails, zoneCopierFunctions } =
+    useCompareContext();
   const [networkSubcategoriesData, setNetworkSubcategoriesData] = useState();
   const {
     isOpen: ErrorPromptIsOpen,
@@ -326,6 +326,279 @@ const NetworkSubcategories = (props) => {
     setNetworkSubcategoriesData();
     getData();
   };
+
+  const bulkCopyHandler = async (
+    data,
+    zoneKeys,
+    credentials,
+    setResults,
+    setProgress
+  ) => {
+    setProgress((prevState) => {
+      // trigger spinner on UI
+      const newState = {
+        ...prevState,
+        [props.id]: {
+          ...prevState[props.id],
+          completed: false,
+        },
+      };
+      return newState;
+    });
+
+    async function sendPostRequest(data, endpoint) {
+      const resp = await patchZoneSetting(data, endpoint);
+      return resp;
+    }
+
+    async function getData() {
+      const resp = await Promise.all([
+        getMultipleZoneSettings(zoneKeys, credentials, "/settings/http2"),
+        getMultipleZoneSettings(zoneKeys, credentials, "/settings/http3"),
+        getMultipleZoneSettings(zoneKeys, credentials, "/settings/0rtt"),
+        getMultipleZoneSettings(zoneKeys, credentials, "/settings/ipv6"),
+        getMultipleZoneSettings(zoneKeys, credentials, "/flags"),
+        getMultipleZoneSettings(zoneKeys, credentials, "/settings/websockets"),
+        getMultipleZoneSettings(
+          zoneKeys,
+          credentials,
+          "/settings/opportunistic_onion"
+        ),
+        getMultipleZoneSettings(zoneKeys, credentials, "/settings/pseudo_ipv4"),
+        getMultipleZoneSettings(
+          zoneKeys,
+          credentials,
+          "/settings/ip_geolocation"
+        ),
+        getMultipleZoneSettings(zoneKeys, credentials, "/settings/max_upload"),
+        getMultipleZoneSettings(
+          zoneKeys,
+          credentials,
+          "/settings/response_buffering"
+        ),
+        getMultipleZoneSettings(
+          zoneKeys,
+          credentials,
+          "/settings/true_client_ip_header"
+        ),
+      ]);
+      const processedResp = resp.map((settingArray) =>
+        settingArray.map((zone) => {
+          if (zone.resp.result?.protocols !== undefined) {
+            let newObj = { ...zone.resp };
+            newObj["result"].value = zone.resp.result.protocols.gRPC;
+            return newObj;
+          } else {
+            return zone.resp;
+          }
+        })
+      );
+      setNetworkSubcategoriesData(processedResp);
+    }
+
+    // not possible for data not to be loaded (logic is at displaying this button)
+    const baseZoneData = data;
+    const otherZoneKeys = zoneKeys.slice(1);
+
+    const subcategories = {
+      http2: undefined,
+      http3: undefined,
+      "0rtt": undefined,
+      ipv6: undefined,
+      grpc: undefined,
+      websockets: undefined,
+      opportunistic_onion: undefined,
+      pseudo_ipv4: undefined,
+      ip_geolocation: undefined,
+      max_upload: undefined,
+      response_buffering: undefined,
+      true_client_ip_header: undefined,
+    };
+
+    const subcategoriesEndpoints = {
+      http2: "/patch/settings/http2",
+      http3: "/patch/settings/http3",
+      "0rtt": "/patch/settings/0rtt",
+      ipv6: "/patch/settings/ipv6",
+      grpc: "/post/flags/products/protocols/changes",
+      websockets: "/patch/settings/websockets",
+      opportunistic_onion: "/patch/settings/opportunistic_onion",
+      pseudo_ipv4: "/patch/settings/pseudo_ipv4",
+      ip_geolocation: "/patch/settings/ip_geolocation",
+      max_upload: "/patch/settings/max_upload",
+      response_buffering: "/patch/settings/response_buffering",
+      true_client_ip_header: "/patch/settings/true_client_ip_header",
+    };
+
+    // initialize state
+    setProgress((prevState) => {
+      const newState = {
+        ...prevState,
+        [props.id]: {
+          ...prevState[props.id],
+          status: "copy",
+          totalToCopy: data.length * data[0].slice(1).length,
+          progressTotal: 0,
+          completed: false,
+        },
+      };
+      return newState;
+    });
+
+    // initialize state
+    setResults((prevState) => {
+      const newState = {
+        ...prevState,
+        [props.id]: {
+          ...prevState[props.id],
+          errors: [],
+          copied: [],
+        },
+      };
+      return newState;
+    });
+
+    for (const record of baseZoneData) {
+      const currentSubcategory = record[0];
+      if (currentSubcategory.success === true && currentSubcategory.result) {
+        if (currentSubcategory.result?.id !== undefined) {
+          // setCurrentProgressSubcategory(currentSubcategory.result.id);
+          const createData = {
+            value: currentSubcategory.result.value,
+          };
+          for (const key of otherZoneKeys) {
+            const dataToCreate = _.cloneDeep(createData);
+            const authObj = {
+              zoneId: credentials[key].zoneId,
+              apiToken: `Bearer ${credentials[key].apiToken}`,
+            };
+            // setCurrentZone(key);
+            const dataWithAuth = { ...authObj, data: dataToCreate };
+            const { resp: postRequestResp } = await sendPostRequest(
+              dataWithAuth,
+              subcategoriesEndpoints[currentSubcategory.result.id]
+            );
+            if (postRequestResp.success === true) {
+              subcategories[currentSubcategory.result.id] = true;
+            } else {
+              const errorObj = {
+                code: postRequestResp.errors[0].code,
+                message: postRequestResp.errors[0].message,
+                data: currentSubcategory.result.id,
+              };
+              setResults((prevState) => {
+                const newState = {
+                  ...prevState,
+                  [props.id]: {
+                    ...prevState[props.id],
+                    errors: prevState[props.id]["errors"].concat(errorObj),
+                  },
+                };
+                return newState;
+              });
+            }
+          }
+        } else if (currentSubcategory.result?.cache !== undefined) {
+          // setCurrentProgressSubcategory("gRPC");
+          const createData = {
+            feature: "gRPC",
+            value: currentSubcategory.result.value, // this case is for gRPC
+          };
+          for (const key of otherZoneKeys) {
+            const dataToCreate = _.cloneDeep(createData);
+            const authObj = {
+              zoneId: credentials[key].zoneId,
+              apiToken: `Bearer ${credentials[key].apiToken}`,
+            };
+            // setCurrentZone(key);
+            const dataWithAuth = { ...authObj, data: dataToCreate };
+            const { resp: postRequestResp } = await sendPostRequest(
+              dataWithAuth,
+              subcategoriesEndpoints.grpc
+            );
+            if (postRequestResp.success === true) {
+              subcategories.grpc = true;
+            } else {
+              const errorObj = {
+                code: postRequestResp.errors[0].code,
+                message: postRequestResp.errors[0].message,
+                data: "ssl_universal",
+              };
+              setResults((prevState) => {
+                const newState = {
+                  ...prevState,
+                  [props.id]: {
+                    ...prevState[props.id],
+                    errors: prevState[props.id]["errors"].concat(errorObj),
+                  },
+                };
+                return newState;
+              });
+            }
+          }
+        } else {
+        }
+      } else {
+      }
+      setProgress((prevState) => {
+        const newState = {
+          ...prevState,
+          [props.id]: {
+            ...prevState[props.id],
+            progressTotal: prevState[props.id]["progressTotal"] + 1,
+          },
+        };
+        return newState;
+      });
+    }
+    setResults((prevState) => {
+      const subcategoriesKeys = Object.keys(subcategories);
+      const copiedData = [];
+      subcategoriesKeys.forEach((k) =>
+        copiedData.push({
+          name: k,
+          success: subcategories[k],
+        })
+      );
+      const newState = {
+        ...prevState,
+        [props.id]: {
+          ...prevState[props.id],
+          copied: copiedData,
+        },
+      };
+      return newState;
+    });
+    setNetworkSubcategoriesData();
+    getData();
+
+    setProgress((prevState) => {
+      const newState = {
+        ...prevState,
+        [props.id]: {
+          ...prevState[props.id],
+          completed: true,
+        },
+      };
+      return newState;
+    });
+    return;
+  };
+
+  if (!networkSubcategoriesData) {
+  } // don't do anything while the app has not loaded
+  else if (networkSubcategoriesData && networkSubcategoriesData.length) {
+    zoneCopierFunctions[props.id] = (setResults, setProgress) =>
+      bulkCopyHandler(
+        networkSubcategoriesData,
+        zoneKeys,
+        credentials,
+        setResults,
+        setProgress
+      );
+  } else {
+    zoneCopierFunctions[props.id] = false;
+  }
 
   return (
     <Stack w="100%" spacing={4}>
